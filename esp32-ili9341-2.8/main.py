37a3390f8757a9567481463e98d070afdf4579bb
JohnO <jouellnyc@gmail.com>
2022-11-19 12:18:43 -0500

    freeze

--- esp32-ili9341-2.8/main.py
+++ esp32-ili9341-2.8/main.py
@@ -1,8 +1,3 @@
-"""
-import mlbapp.esp32_oled as myoled 
-import mlbapp.err2oled
-"""
-
 import os
 
 def file_or_dir_exists(filename):
@@ -13,18 +8,15 @@ def file_or_dir_exists(filename):
     else:
         return True
 
-mode='debug'
+mode='normal'
 
-if mode=="normal":
+if mode=="debug":
 
     pass
 
 else:
 
-#    try:
-        
     if file_or_dir_exists('/appsetup/setup_complete.txt'):        
-
 
         """ NETWORK SETUP """
         import mlbapp.network_setup
@@ -40,13 +32,3 @@ else:
     else:
 
         import appsetup.setup.py
-        
-"""
-    except Exception as e:
-        #myoled.oled.fill(0)
-        #myoled.oled.show()
-        #mlbapp.err2oled.printout(str(e),myoled.oled)
-        #myoled.oled.show()
-        print(e)   
-"""
-
