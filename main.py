068f1a2995c72517550cae9963a5d6929af88152
JohnO <jouellnyc@gmail.com>
2022-12-11 16:07:05 -0500

    RC1

--- main.py
+++ main.py
@@ -3,10 +3,15 @@
 
 from hardware.err_to_screen import errr
 from hardware.config import screen 
+from hardware.esp32_oled_2_8_inch import print_setup
+
+print_setup("Booting Up...")
 
 try:
     
     import os
+    import time
+    time.sleep(2)
 
     def file_or_dir_exists(filename):
         try:
@@ -27,24 +32,26 @@ try:
         if file_or_dir_exists('/appsetup/setup_complete.txt'):        
 
             """ NETWORK SETUP """
+            print_setup("Network Setup ...")
             import hardware.network_setup
 
             """ NTP SETUP """
+            print_setup("Time Setup ...")
             import hardware.ntp_setup
             hardware.ntp_setup.main()
 
             """ Run MLB APP """
+            print_setup("Launch MLB Kiosk ")
             if screen == 'ili9341':
                 import mlbapp.mlb_app_runner_ili9341 as mlb_app_runner
             elif screen == 'oled': 
                 import mlbapp.mlb_app_runner_oled as mlb_app_runner
 
         else:
-
+            print_setup("Running Setup ...")
             import appsetup.setup
 
 except Exception as e:
     
     from hardware.err_to_screen import print_err
-    print_err(e)
-    
+    print_err(e)    
