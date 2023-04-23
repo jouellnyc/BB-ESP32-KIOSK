814ff0f9e9798e3935dc98cf0a7b0c01c9df8c69
JohnO <jouellnyc@gmail.com>
2023-04-23 16:46:53 -0400

    functions

--- bbapp/test_colors.py
+++ bbapp/test_colors.py
@@ -1,19 +1,33 @@
 import ujson
 
-with open("/bbapp/team_ids.py") as f:
-    at = ujson.loads(f.read())
+def show_teams():
+    with open("/bbapp/team_ids.py") as f:
+        at = ujson.loads(f.read())
 
-for team in at['teams']:
-    print(team)
+    for team in at['teams']:
+        print(team)
     
     
-from bbapp.team_colors import TEAM_COLORS
-for t, c in sorted( TEAM_COLORS.items()):
-    print(t, c)
-    
+    from bbapp.team_colors import TEAM_COLORS
+    for t, c in sorted( TEAM_COLORS.items()):
+        print(t, c)
 
-for team in sorted(at['teams'], key=lambda d: d['teamCode']): 
-    if team['teamCode'].upper() in TEAM_COLORS.keys():
-        print(f"{team['teamCode'].upper()} OK")
-    else:
-        print(f"{team['teamCode'].upper()} NOTOK")
+def grep_team(letter):
+    for team in at['teams']:
+        if letter in team['teamName']:
+            print(f"team_id={team['id']}")
+            print(f"team_name={team['teamName']}")
+            print(f"team_code={team['teamCode'].upper()}")
+            print("")
+def main()
+    pass
+
+"""
+from bbapp.test_colors import show_teams
+bbapp.test_colors.show_teams('y')
+"""
+
+if __name__ == "__main__":
+    main()
+    
+    
