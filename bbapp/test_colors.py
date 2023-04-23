b8828f61757209d3b71a4644e61483e998fc5609
JohnO <jouellnyc@gmail.com>
2023-04-23 16:55:25 -0400

    Separte Main Dictionary out

--- bbapp/test_colors.py
+++ bbapp/test_colors.py
@@ -1,12 +1,12 @@
 import ujson
 
-def show_teams():
-    with open("/bbapp/team_ids.py") as f:
+with open("/bbapp/team_ids.py") as f:
         at = ujson.loads(f.read())
-
+        
+def show_teams():
+    
     for team in at['teams']:
         print(team)
-    
     
     from bbapp.team_colors import TEAM_COLORS
     for t, c in sorted( TEAM_COLORS.items()):
@@ -16,18 +16,12 @@ def grep_team(letter):
     for team in at['teams']:
         if letter in team['teamName']:
             print(f"team_id={team['id']}")
-            print(f"team_name={team['teamName']}")
-            print(f"team_code={team['teamCode'].upper()}")
+            print(f"team_name='{team['teamName']}'")
+            print(f"team_code='{team['teamCode'].upper()}'")
             print("")
-def main()
-    pass
-
+            
 """
 from bbapp.test_colors import show_teams
 bbapp.test_colors.show_teams('y')
-"""
-
-if __name__ == "__main__":
-    main()
-    
+"""    
     
