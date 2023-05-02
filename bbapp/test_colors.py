1611e9646105ca508f20613043d28c0c66446ff9
JohnO <jouellnyc@gmail.com>
2023-05-02 17:56:41 -0400

    Show actual Colors

--- bbapp/test_colors.py
+++ bbapp/test_colors.py
@@ -1,3 +1,4 @@
+import time
 import ujson
 
 with open("/bbapp/team_ids.py") as f:
@@ -5,12 +6,21 @@ with open("/bbapp/team_ids.py") as f:
         
 def show_teams():
     
+    from hardware.screen_runner import display as d
+    from hardware.ili9341 import color565
+
     for team in at['teams']:
         print(team)
     
     from bbapp.team_colors import TEAM_COLORS
+    
     for t, c in sorted( TEAM_COLORS.items()):
         print(t, c)
+        d.fresh_box()    
+        r, g, b = c
+        color = color565(r, g, b)
+        d.draw_text(5, 100, f"{t}:5 "   , d.score_font, d.white , color)
+        time.sleep(1)
 
 def grep_team(letter):
     for team in at['teams']:
